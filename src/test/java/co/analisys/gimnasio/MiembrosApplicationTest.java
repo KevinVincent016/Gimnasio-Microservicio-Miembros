dbf93267b85fe4f203020c3396f96d3b37aa1a4c
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-08-24 00:01:10 -0500

    Se añadió archivo test por defecto

---
+++ src/test/java/co/analisys/gimnasio/MiembrosApplicationTest.java
@@ -0,0 +1,13 @@
+package co.analisys.gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MiembrosApplicationTest {
+    
+    @Test
+	void contextLoads() {
+	}
+
+}
